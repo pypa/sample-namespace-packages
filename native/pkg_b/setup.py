725e2a8da8c915aa902e32884b01aaa1625ceafa
chrysle <i.kant@posteo.de>
2023-11-29 16:07:14 +0100

    Migrate examples to `src-layout`

--- native/pkg_b/setup.py
+++ native/pkg_b/setup.py
@@ -27,7 +27,8 @@ setup(
     author_email='jonwayne@google.com',
 
     license='Apache Software License',
-
-    packages=['example_pkg.b'],
+    # This example uses automatic discovery. You can also implement custom discovery like this:
+    # packages=find_namespace_packages(where='src', include=['example_pkg.b']),
+    # package_dir={'': 'src'},
     zip_safe=False,
 )
